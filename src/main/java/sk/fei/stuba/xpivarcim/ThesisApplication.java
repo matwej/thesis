401aa0181d82e95030b888f41c33afab5632f6cf
pivarci <pivarci.matej@gmail.com>
2016-04-11 15:49:45 +0200

    gradle build zmena

--- src/main/java/sk/fei/stuba/xpivarcim/ThesisApplication.java
+++ src/main/java/sk/fei/stuba/xpivarcim/ThesisApplication.java
@@ -3,6 +3,8 @@ package sk.fei.stuba.xpivarcim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class ThesisApplication {
 
