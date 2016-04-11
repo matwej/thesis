a1a6e2d8bbea28dc7163b845cc3264b7661c0ff1
pivarci <pivarci.matej@gmail.com>
2016-04-11 15:38:39 +0200

    init commit

---
+++ src/main/java/sk/fei/stuba/xpivarcim/ThesisApplication.java
@@ -0,0 +1,12 @@
+package sk.fei.stuba.xpivarcim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisApplication.class, args);
+	}
+}
