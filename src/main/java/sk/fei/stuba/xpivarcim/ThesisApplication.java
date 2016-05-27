bbf5a11e6d9c893a75b8094de969dcbc84d19673
pivarci <pivarci.matej@gmail.com>
2016-05-27 03:21:33 +0200

    cleaning

--- src/main/java/sk/fei/stuba/xpivarcim/ThesisApplication.java
+++ src/main/java/sk/fei/stuba/xpivarcim/ThesisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThesisApplication {
-    // TODO upravit jpa styl schemy pred "kolaudovanim"
 	public static void main(String[] args) {
         SpringApplication.run(ThesisApplication.class, args);
     }
